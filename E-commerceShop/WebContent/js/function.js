3ff9bd80ee31549ae300efc55e0554d2968c4ada
Yuweiluo1618 <yuwei.luo@alumni.ubc.ca>
2020-03-19 01:09:38 -0700

    done

--- E-commerceShop/WebContent/js/function.js
+++ E-commerceShop/WebContent/js/function.js
@@ -13,5 +13,49 @@ function FocusItem(obj){
 
 function CheckItem(obj){
 	
+	var msgBox = $(obj).next('span');
+	
+	switch($(obj).attr('name')){
+		case "username":
+			if(obj.value == ""){
+				msgBox.html('required username');
+				msgBox.addClass('error');
+			}
+			break;
+		
+		case "name":
+			if(obj.value == ""){
+				msgBox.html('required name');
+				msgBox.addClass('error');
+			}
+			break;
+		
+		case "password":
+			if(obj.value == ""){
+				msgBox.html('required passworrd');
+				msgBox.addClass('error');
+			}
+			break;
+		
+		case"repassword":
+			if(obj.value == ""){
+				msgBox.html('required repassworrd');
+				msgBox.addClass('error');
+			}else if($(obj).val() != $('input[name="password"]').val()){
+				msgBox.html('password not match');
+				msgBox.addClass('error');
+			}
+			break;
+		
+		case"verycode":
+			var numShow = $(obj).next().next();
+			if(obj.value == ""){
+				numShow.html('required verify code');
+				numShow.addClass('error');
+			}
+			
+			break;
+	
+	}
 	
 }
